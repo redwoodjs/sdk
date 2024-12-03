bd523ce09e926053640d77f02acf1949a91d416d
Justin van der Merwe <justnvdm@gmail.com>
2024-12-03 12:11:31 +0200

    rm todo

--- experiments/rsc/src/app/components/Like.tsx
+++ experiments/rsc/src/app/components/Like.tsx
@@ -2,6 +2,5 @@
 import { increment } from "../counter";
 
 export function Like() {
-  // todo(justinvdm, 2024-11-28): Replace with 'use client' equivalent
   return <button onClick={() => increment()}>+</button>;
 }
