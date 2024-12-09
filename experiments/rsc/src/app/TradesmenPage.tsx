92c2bbe651cd7eb58e9d428e6d44f58a62c01ea0
Herman Stander <her.stander@gmail.com>
2024-12-09 13:31:35 +0200

    update button style

--- experiments/rsc/src/app/TradesmenPage.tsx
+++ experiments/rsc/src/app/TradesmenPage.tsx
@@ -8,9 +8,7 @@ export default async function TradesmenPage() {
       <h1 className="text-3xl text-gray-800 font-bold text-center mb-4">What kind of tradesman are you looking for?</h1>
       <div className="flex flex-col items-center justify-center w-full">
         {uniqueProfessions.map((profession) => (
-          <div key={profession}>
-            <a className="text-sm font-bold text-gray-600 hover:text-gray-800 bg-gray-100 p-4 rounded-md mb-2 w-full" href={`/tradesmen/${profession}`}>{profession}</a>
-          </div>
+          <a key={profession} className="text-sm font-bold text-gray-600 hover:text-gray-800 bg-gray-100 p-4 rounded-md mb-2 w-full" href={`/tradesmen/${profession}`}>{profession}</a>
         ))}
       </div>
     </div>
