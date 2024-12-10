5557234cf3e8b65e2948a3929230375f71f9a726
Herman Stander <her.stander@gmail.com>
2024-12-10 09:59:32 +0200

    remove debugging logs

--- experiments/rsc/src/app/createTradesman.ts
+++ experiments/rsc/src/app/createTradesman.ts
@@ -10,16 +10,8 @@ export async function createTradesman(formData: FormData) {
   if (!profilePicture) {
     throw new Error("profilePicture is required.");
   }
-
   const filename = (name as string)?.split(" ").join("-").toLowerCase() + ".jpg";
-
   const file = new File([profilePicture], filename, { type: "image/jpeg" });
-
-  console.log("File size:", file.size); // Verify the file size
-  console.log("File type:", file.type); // Verify the file type
-
-  
-
   const profilePictureUrl = await R2Storage.uploadFile(file, filename);
 
   await db
