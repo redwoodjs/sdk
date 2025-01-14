3c6c71268ad1e0a591ea17a120fbeac4035f5699
Peter Pistorius <peter.pistorius@gmail.com>
2025-01-14 13:12:07 +0200

    Add save function.

--- experiments/billable/src/app/pages/invoiceDetail/functions.ts
+++ experiments/billable/src/app/pages/invoiceDetail/functions.ts
@@ -3,85 +3,24 @@ import {
   type Invoice,
 } from "@prisma/client";
 import { db } from "../../../db";
+import { InvoiceItem, InvoiceTaxes } from "../../services/invoices";
 
-export async function saveInvoice(
-  id: number,
-  // invoice: Partial<Invoice>,
-  // items: InvoiceItem[],
-  // taxes: InvoiceTaxItem[],
-) {
-  // // use zod to validate?
-  // const {
-  //   title,
-  //   number,
-  //   date,
-  //   supplierName,
-  //   supplierContact,
-  //   customer,
-  //   notesA,
-  //   notesB,
-  // } = invoice;
+export async function saveInvoice(id: number, invoice: Omit<Invoice, 'items' | 'taxes'>, items: InvoiceItem[], taxes: InvoiceTaxes[]) {
 
-  // // is this a new invoice?
-  // let exists = false
-  // const n = await db.invoice.count({ where: { id }})
-  // if (n > 0) {
-  //   exists = true
-  // }
+  // validate input with zod
+  // validate user id.
 
-  // if (!exists) {
-  //   // get next invoice number
-  //   const numOfInvoices = await db.invoice.count({ where: { userId: 1 }})
-  //   const invoice = await db.invoice.create({
-  //     data: {
-  //       title,
-  //       number: number ?? (numOfInvoices + 1).toString(),
-  //       date,
-  //       supplierName,
-  //       supplierContact,
-  //       customer,
-  //       notesA,
-  //       notesB,
-  //     }
-  //   })
+  const data: Invoice = {
+    ...invoice,
+    items: JSON.stringify(items),
+    taxes: JSON.stringify(taxes),
+  }
 
-  //   // create invoices
-  //   // create taxes
-  // } else {
-
-  //   const invoice = await db.invoice.update({
-  //     data: {
-  //       title,
-  //       number,
-  //       date,
-  //       supplierName,
-  //       supplierContact,
-  //       customer,
-  //       notesA,
-  //       notesB
-  //     },
-  //     where:
-  //       { id }
-  //     })
-
-  //   for (const item of items) {
-  //     if (item.id) {
-  //       await db.invoiceItem.update({
-  //         data: item,
-  //         where: {
-  //           id: item.id
-  //         }
-  //       })
-  //     } else {
-  //       await db.invoiceItem.create({
-  //         data: { ...item, invoiceId: invoice.id, id: undefined },
-  //       })
-  //     }
-  //   }
-  // }
-}
-
-export async function deleteInvoiceItem(id: number) {
-  // validate that the user can actually do this.
-  // await db.invoiceItem.delete({ where: { id }})
+  await db.invoice.upsert({
+    create: data,
+    update: data,
+    where: {
+      id,
+    }
+  })
 }
