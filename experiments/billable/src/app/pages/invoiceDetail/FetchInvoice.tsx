ac2b12e8543cc124c02f6c20f94daa1ed94e4c80
Peter Pistorius <peter.pistorius@gmail.com>
2025-01-14 13:46:39 +0200

    Switch from integers to UUIDs for primary keys.

--- experiments/billable/src/app/pages/invoiceDetail/FetchInvoice.tsx
+++ experiments/billable/src/app/pages/invoiceDetail/FetchInvoice.tsx
@@ -2,7 +2,7 @@
 
 import { getInvoice } from "../../services/invoices";
 
-export async function FetchInvoice(props: { id: number, children: Function }) {
+export async function FetchInvoice(props: { id: string, children: Function }) {
   const invoice = await getInvoice(props.id, 1);
   return props.children(invoice)
 }
