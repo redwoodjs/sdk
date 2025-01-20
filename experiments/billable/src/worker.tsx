de464c2f98ccd703f4eb932033aa54a341f8b0b0
Peter Pistorius <peter.pistorius@gmail.com>
2025-01-20 11:27:32 +0200

    Confirm change.

--- experiments/billable/src/worker.tsx
+++ experiments/billable/src/worker.tsx
@@ -9,8 +9,8 @@ import { renderToRscStream } from "./render/renderToRscStream";
 import { ssrWebpackRequire } from "./imports/worker";
 import { rscActionHandler } from "./register/worker";
 import { setupR2Storage } from "./r2storage";
-import InvoiceListPage from "./app/pages/InvoiceList/InvoiceListPage";
-import InvoiceDetailPage from "./app/pages/invoiceDetail/Page";
+import InvoiceListPage from "./app/InvoiceListPage";
+import InvoiceDetailPage from "./app/pages/InvoiceDetail/InvoiceDetailPage";
 import InvoicePdfPage from "./app/pages/invoicePdf/Page"
 import { ErrorResponse } from './error';
 import { enforceUserLoggedIn, getSession, performLogin } from './auth';
