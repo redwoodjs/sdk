588d5ba442d7f49bc8b8f06c7e035ab0856317f2
Peter Pistorius <peter.pistorius@gmail.com>
2025-01-22 16:19:39 +0200

    Add this.

--- experiments/billable/worker-configuration.d.ts
+++ experiments/billable/worker-configuration.d.ts
@@ -2,7 +2,7 @@
 
 interface Env {
 	SECRET_KEY: "secret";
-	SESSION_DO: DurableObjectNamespace<import("./dist/worker/worker").SessionDO>;
+	SESSION_DO: DurableObjectNamespace /* SessionDO */;
 	R2: R2Bucket;
 	DB: D1Database;
 	ASSETS: Fetcher;
