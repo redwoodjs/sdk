8e08931f7a182c8821e18b2ba3b3ead6ce2391c5
Peter Pistorius <peter.pistorius@gmail.com>
2025-02-06 11:31:43 +0200

    Change border colour to something a bit dark.

--- experiments/billable/tailwind.config.js
+++ experiments/billable/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        border: "hsl(var(--border))",
+        border: "#333",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
