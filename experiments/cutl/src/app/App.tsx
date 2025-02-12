40a4561a45613ce04ca25b126ec26901150441b6
Herman Stander <her.stander@gmail.com>
2025-02-12 14:57:29 +0200

    added domain and chnaged name

--- experiments/cutl/src/app/App.tsx
+++ experiments/cutl/src/app/App.tsx
@@ -11,7 +11,7 @@ const vitePreamble = `\
 export const App: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en">
     <head>
-      <title>Cuttable: Plan. Cut. Optimize.</title>
+      <title>Cutable: Plan. Cut. Optimize.</title>
       {import.meta.env.DEV && !process.env.PREVIEW ? (
         <script
           type="module"
