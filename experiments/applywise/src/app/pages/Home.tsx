d2ce6bd4159f1d0843720e6337f686bb1b57d5ac
Amy Haywood Dutton <me@amyhaywood.com>
2025-02-18 10:01:04 -0600

    🚧 WIP: Trying to make a db call

--- experiments/applywise/src/app/pages/Home.tsx
+++ experiments/applywise/src/app/pages/Home.tsx
@@ -1,8 +1,30 @@
+import { db } from "../../db";
 
-export function Home() {
+interface User {
+  id: string;
+  email: string;
+}
+
+export async function Home() {
+  const getUsers = async (): Promise<User[]> => {
+    const users =
+      (await db.user.findMany({
+        select: {
+          id: true,
+          email: true
+        },
+      })) ?? [];
+    return users;
+  }
+
+  const users = await getUsers();
+
   return (
     <div>
       <h1>Hello World</h1>
+      {users.map((user: User) => (
+        <div key={user.id}>{user.email}</div>
+      ))}
     </div>
   );
 }
