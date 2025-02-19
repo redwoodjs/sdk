7ac73af2d421d1539df9ea510ad8f2dd79cf4551
Amy Haywood Dutton <me@amyhaywood.com>
2025-02-19 08:38:06 -0600

    ✨ Installed Tailwind v4

--- experiments/applywise/src/app/Document.tsx
+++ experiments/applywise/src/app/Document.tsx
@@ -3,8 +3,9 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>@redwoodjs/starter-minimal</title>
+      <title>Applywise</title>
       <script type="module" src="/src/client.tsx"></script>
+      <link rel="stylesheet" href="/src/app/styles.css" />
     </head>
     <body>
       <div id="root">{children}</div>
