11feced1ab2aa8a046b9e512f497a39083c20474
Herman Stander <her.stander@gmail.com>
2025-02-19 08:55:19 +0200

    fix js

--- experiments/cutable/src/app/Document.tsx
+++ experiments/cutable/src/app/Document.tsx
@@ -17,13 +17,13 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <title>Cutable: Plan. Cut. Optimize.</title>
       <link rel="stylesheet" href={stylesUrl} />
       <script type="module" src="/src/client.tsx"></script>
-      <script type="text/javascript">
+      <script type="text/javascript">{`
           (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "qc5fhoezed");
-      </script>
+      `}</script>
     </head>
     <body>
       <div id="root">{children}</div>
