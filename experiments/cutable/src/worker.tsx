a0812fbe880c68448b5c9809ed8589766e9364bc
Herman Stander <her.stander@gmail.com>
2025-02-19 10:15:38 +0200

    deploy issues

--- experiments/cutable/src/worker.tsx
+++ experiments/cutable/src/worker.tsx
@@ -3,14 +3,20 @@ import { index, layout } from '@redwoodjs/sdk/router';
 import { Document } from 'src/Document';
 import HomePage from 'src/pages/home/HomePage';
 
+export { SessionDO } from "./session";
+
 type Context = {
+  foo: number
 }
 
 export default defineApp<Context>([
-  ({ ctx }) => {
-    // setup ctx here
-    ctx;
+  // >>> Replaces `getContext()`
+  ({ ctx }: { ctx: Context }) => {
+    // >>> You can do side effects (e.g, setup like `setupDb()`) here
+    // >>> You can set your context here
+    ctx.foo = 23
   },
+  // @ts-ignore
   layout(Document, [
     index([
       HomePage,
