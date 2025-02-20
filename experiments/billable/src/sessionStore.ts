f76e03245aa7668e82a7e56639c08d0aef9914e7
justinvdm <justinvderm@gmail.com>
2025-02-20 13:35:18 +0200

    fix

--- experiments/billable/src/sessionStore.ts
+++ experiments/billable/src/sessionStore.ts
@@ -5,7 +5,7 @@ export let sessions: ReturnType<typeof defineDurableSession>;
 export const setupSessionStore = (env: Env) => {
   sessions = defineDurableSession({
     secretKey: env.SECRET_KEY,
-    sessionDurableObject: env.SESSION_DURABLE_OBJECT,
+    sessionDurableObject: env.SESSION_DO
   })
 
   return sessions;
