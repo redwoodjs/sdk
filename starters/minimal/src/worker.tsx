fdbb11065b36d454618f17a8f01f1d1229fe1ba4
Peter Pistorius <peter.pistorius@gmail.com>
2025-02-24 10:01:32 +0200

    Restore template. Whoops.

--- starters/minimal/src/worker.tsx
+++ starters/minimal/src/worker.tsx
@@ -1,47 +1,19 @@
-import { defineApp } from "@redwoodjs/sdk/worker";
-import { index, layout, route } from "@redwoodjs/sdk/router";
-import { Document } from "src/Document";
+import { defineApp } from '@redwoodjs/sdk/worker';
+import { index, layout } from '@redwoodjs/sdk/router';
+import { Document } from 'src/Document';
+import { Home } from 'src/pages/Home';
 
-function Home() {
-  return (
-    <div>
-      <b>Hello, world!</b>
-    </div>
-  );
+type Context = {
 }
 
 export default defineApp<Context>([
-  route("/", [() => new Response("1"), () => new Response("2")]),
-  route("/r/:id/:slug", ({ request, params }) => {
-    return <div>Hello, world!</div>;
-  }),
-  route("/primes", () => {
-    function generatePrimes(limit = 100) {
-      const primes: number[] = [];
-      for (let num = 2; num <= limit; num++) {
-        let isPrime = true;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
-          if (num % i === 0) {
-            isPrime = false;
-            break;
-          }
-        }
-        if (isPrime) primes.push(num);
-      }
-      return primes;
-    }
-
-    const primeNumbers = generatePrimes();
-    return (
-      <div>
-        <h2>First 100 Prime Numbers</h2>
-        <pre>{JSON.stringify(primeNumbers, null, 2)}</pre>
-      </div>
-    );
-  }),
-  // layout(Document, [
-  //   index([
-  //     Home,
-  //   ]),
-  // ]),
-]);
+  ({ ctx }) => {
+    // setup ctx here
+    ctx;
+  },
+  layout(Document, [
+    index([
+      Home,
+    ]),
+  ]),
+])
