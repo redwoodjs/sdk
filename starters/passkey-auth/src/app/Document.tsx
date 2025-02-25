8341fa3bcff871207a433d5de9d4d6813f515d33
justinvdm <justinvderm@gmail.com>
2025-02-25 11:23:06 +0200

    fix: Small fixes and comments for passkey auth

--- starters/passkey-auth/src/app/Document.tsx
+++ starters/passkey-auth/src/app/Document.tsx
@@ -3,7 +3,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>@redwoodjs/starter-minimal</title>
+      <title>@redwoodjs/starter-passkey-auth</title>
       <script type="module" src="/src/client.tsx"></script>
     </head>
     <body>
