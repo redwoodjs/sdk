8c0f62ffd598ed7bb6fc9effbf1181139f823e06
Amy Haywood Dutton <me@amyhaywood.com>
2025-03-05 20:41:26 -0600

    💬 Updates to the tutorial
    - Made tweaks to the first 3 “lessons”
    - Added a link to the GitHub repo, at the bottom of all the tutorial pages
    - Updated the line number default setting (off by default, on for js, ts, css, tsx, jsx by default)

--- docs/ec.config.mjs
+++ docs/ec.config.mjs
@@ -3,4 +3,14 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   plugins: [pluginLineNumbers()],
+  defaultProps: {
+    // disable line numbers by default
+    showLineNumbers: false,
+    // But enable line numbers for certain languages
+    overridesByLang: {
+      'js,ts,css,tsx,jsx': {
+        showLineNumbers: true,
+      }
+    }
+  }
 }
