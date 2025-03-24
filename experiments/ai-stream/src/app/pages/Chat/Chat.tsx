b1d35d493398f13297eb403612bade27fe750fb4
justinvdm <justinvderm@gmail.com>
2025-03-24 13:28:34 +0200

    useEventStream

--- experiments/ai-stream/src/app/pages/Chat/Chat.tsx
+++ experiments/ai-stream/src/app/pages/Chat/Chat.tsx
@@ -1,9 +1,32 @@
-import { ChatBar } from "./ChatBar";
+"use client";
 
-export async function Chat() {
+import { sendMessage } from "./functions";
+import { useState } from "react";
+import { useEventStream } from "@redwoodjs/sdk/client";
+
+export function Chat() {
+  const [message, setMessage] = useState("");
+  const [reply, setReply] = useState("");
+  const eventStream = useEventStream({
+    onEvent: (event) => {
+      setReply((prev) => prev + JSON.parse(event.data).response);
+    },
+  });
+
+  const onClick = async () => {
+    setReply(""); // Reset reply before new message
+    (await sendMessage(message)).pipeTo(eventStream());
+  };
+
   return (
     <div>
-      <ChatBar />
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={onClick}>Send</button>
+      <div>{reply}</div>
     </div>
   );
 }
