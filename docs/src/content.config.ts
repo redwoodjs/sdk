2cbecbab9354a04317a58770a31bf8a61cb7845b
Amy Haywood Dutton <me@amyhaywood.com>
2025-04-03 21:55:20 -0500

    ✨ Added a preview banner

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,18 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        banner: z.object({
+          content: z.string(),
+        }).default({
+          content: 'RedwoodSDK is in preview mode.',
+        })
+      }),
+    }),
+  }),
 };
