839b0c269dc427565a9f1a7551ae9fce7e0b7d18
justinvdm <justinvderm@gmail.com>
2025-04-07 03:35:47 +0200

    progress

--- sdk/src/runtime/lib/realtime/worker.ts
+++ sdk/src/runtime/lib/realtime/worker.ts
@@ -2,15 +2,18 @@ import { route } from "../../entries/router";
 import { validateUpgradeRequest } from "./validateUpgradeRequest";
 import type { RealtimeDurableObject } from "./durableObject";
 import { DEFAULT_REALTIME_KEY } from "./constants";
+import { requestContext } from "../../requestContext/worker";
+import { env } from "cloudflare:workers";
 
 export { renderRealtimeClients } from "./renderRealtimeClients";
 
 export const realtimeRoute = (
   getDurableObjectNamespace: (
-    env: Env,
+    env: Cloudflare.Env,
   ) => DurableObjectNamespace<RealtimeDurableObject>,
 ) =>
-  route("/__realtime", async function ({ request, env }) {
+  route("/__realtime", async function () {
+    const { request } = requestContext;
     const validation = validateUpgradeRequest(request);
 
     if (!validation.valid) {
