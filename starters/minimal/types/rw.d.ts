7c124bcd8fbd1ea31a3b268d8d10fb7e3d80c37a
justinvdm <justinvderm@gmail.com>
2025-04-08 16:20:02 +0200

    progress

--- starters/minimal/types/rw.d.ts
+++ starters/minimal/types/rw.d.ts
@@ -1,8 +1,10 @@
 import { RequestContext } from "@redwoodjs/sdk/worker";
-import { Data } from "@/worker";
+import { Data, AppContext } from "@/worker";
 
 declare module "@redwoodjs/sdk/worker" {
   export const requestContext: RequestContext & {
     data: Data;
   };
+
+  export interface DefaultAppContext extends AppContext {}
 }
