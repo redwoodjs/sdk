68c6648d3cf8b0b1edf747e262c24e8902a89efb
justinvdm <justinvderm@gmail.com>
2025-04-08 16:24:28 +0200

    progress

--- starters/sessions/types/rw.d.ts
+++
@@ -1,8 +0,0 @@
-import { RequestContext } from "@redwoodjs/sdk/worker";
-import { Data } from "@/worker";
-
-declare module "@redwoodjs/sdk/worker" {
-  export const requestContext: RequestContext & {
-    data: Data;
-  };
-}
