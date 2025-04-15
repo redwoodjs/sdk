7f1a58f03af058f93e2065a3a23eebd3f5fce484
justinvdm <justinvderm@gmail.com>
2025-04-15 15:56:01 +0200

    progress

--- starters/standard/src/app/Document.tsx
+++ starters/standard/src/app/Document.tsx
@@ -1,27 +1,17 @@
-<<<<<<< Updated upstream
-import { TurnstileScript } from "@redwoodjs/sdk/turnstile";
-
-export const Document: React.FC<{ children: React.ReactNode }> = ({
-  children,
-=======
 import { DocumentProps } from "@redwoodjs/sdk/router";
+import { TurnstileScript } from "@redwoodjs/sdk/turnstile";
 
 export const Document: React.FC<DocumentProps> = ({
   children,
   rw: { nonce },
->>>>>>> Stashed changes
 }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
-<<<<<<< Updated upstream
       <TurnstileScript />
-      <script type="module" src="/src/client.tsx"></script>
-=======
       <script nonce={nonce}>import("./src/client.tsx")</script>
->>>>>>> Stashed changes
     </head>
     <body>
       <div id="root">{children}</div>
