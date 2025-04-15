8520d16b6654a49fa259cedcde7054fccc3fa067
justinvdm <justinvderm@gmail.com>
2025-04-15 21:22:18 +0200

    fix: Fix client side loading after 0.0.59

--- starters/standard/src/app/Document.tsx
+++ starters/standard/src/app/Document.tsx
@@ -9,7 +9,7 @@ export const Document: React.FC<DocumentProps> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
-      <script src="/src/client.tsx"></script>
+      <script type="module" src="/src/client.tsx"></script>
     </head>
     <body>
       <div id="root">{children}</div>
