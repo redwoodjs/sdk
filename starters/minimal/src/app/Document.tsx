a17dbe07ae2dafeca76bfa055ef52aba9ac270db
justinvdm <justinvderm@gmail.com>
2025-04-16 03:05:23 +0200

    feat: Preload entry point

--- starters/minimal/src/app/Document.tsx
+++ starters/minimal/src/app/Document.tsx
@@ -6,6 +6,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-minimal</title>
+      <link rel="preload" href="/src/client.tsx" as="script" />
     </head>
     <body>
       <div id="root">{children}</div>
