d8b06326208a5120f4eab6cfd45af91f320c0b04
justinvdm <justinvderm@gmail.com>
2025-04-17 03:59:14 +0200

    progress

--- sdk/src/runtime/client.tsx
+++ sdk/src/runtime/client.tsx
@@ -17,13 +17,13 @@ type TransportContext = {
 export type Transport = (context: TransportContext) => CallServerCallback;
 
 export type CreateCallServer = (
-  context: TransportContext,
+  context: TransportContext
 ) => <Result>(id: null | string, args: null | unknown[]) => Promise<Result>;
 
 export const fetchTransport: Transport = (transportContext) => {
   const fetchCallServer = async <Result,>(
     id: null | string,
-    args: null | unknown[],
+    args: null | unknown[]
   ): Promise<Result> => {
     const { createFromFetch, encodeReply } = await import(
       "react-server-dom-webpack/client.browser"
@@ -41,7 +41,7 @@ export const fetchTransport: Transport = (transportContext) => {
         method: "POST",
         body: args != null ? await encodeReply(args) : null,
       }),
-      { callServer: fetchCallServer },
+      { callServer: fetchCallServer }
     ) as Promise<ActionResponse<Result>>;
 
     transportContext.setRscPayload(streamData);
@@ -73,8 +73,9 @@ export const initClient = async ({
   const React = await import("react");
   const { hydrateRoot } = await import("react-dom/client");
   // @ts-ignore: todo(peterp, 2024-11-27): Type these properly.
-  const { createFromReadableStream, createFromFetch, encodeReply } =
-    await import("react-server-dom-webpack/client.browser");
+  const { createFromReadableStream } = await import(
+    "react-server-dom-webpack/client.browser"
+  );
   const { rscStream } = await import("rsc-html-stream/client");
 
   let rscPayload: any;
--- sdk/vendor/src/react-dom.client.js
+++ sdk/vendor/src/react-dom.client.js
@@ -1,1 +1,7 @@
+import ReactDOMDefault from "react-dom";
+
 export * from "react-dom";
+const __DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE =
+  ReactDOMDefault.__DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;
+export { __DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE };
+export default ReactDOMDefault;
