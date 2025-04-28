8009d77ea44dda3ac60a58ae5ecc1c60ac33ec56
justinvdm <justinvderm@gmail.com>
2025-04-28 10:13:06 +0200

    progress

--- starters/standard/src/app/Document.tsx
+++ starters/standard/src/app/Document.tsx
@@ -6,7 +6,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
-      <link rel="preload" href="/src/client.tsx" as="script" />
+      <link rel="modulepreload" href="/src/client.tsx" />
     </head>
     <body>
       <div id="root">{children}</div>
