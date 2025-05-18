dfb8620be253dcb8b1158e44f90552badecc8a94
justinvdm <justinvderm@gmail.com>
2025-05-18 05:20:51 +0200

    progress

--- sdk/src/runtime/render/__rwsdkssr_render.tsx
+++ sdk/src/runtime/render/__rwsdkssr_render.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactServerDom from "react-dom/server.edge";
 import { type DocumentProps } from "../lib/router";
 import { type RequestInfo } from "../requestInfo/types";
-import { AsyncResource } from "async_hooks";
 
 const { use } = React;
 const { renderToReadableStream } = ReactServerDom;
 
-export const renderRscThenableToHtmlStream = ({
+export const renderRscThenableToHtmlStream = async ({
   thenable,
   Document,
   requestInfo,
@@ -16,19 +15,15 @@ export const renderRscThenableToHtmlStream = ({
   Document: React.FC<DocumentProps>;
   requestInfo: RequestInfo;
 }) => {
-  const resource = new AsyncResource("SSRStreamer");
+  const Component = () => {
+    return (
+      <Document {...requestInfo}>
+        {(use(thenable) as { node: React.ReactNode }).node}
+      </Document>
+    );
+  };
 
-  return resource.runInAsyncScope(() => {
-    const Component = () => {
-      return (
-        <Document {...requestInfo}>
-          {(use(thenable) as { node: React.ReactNode }).node}
-        </Document>
-      );
-    };
-
-    return renderToReadableStream(<Component />, {
-      nonce: requestInfo.rw.nonce,
-    });
+  return await renderToReadableStream(<Component />, {
+    nonce: requestInfo.rw.nonce,
   });
 };
