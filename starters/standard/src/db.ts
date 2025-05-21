252d63acc5936f1a8c28500b3125ab07dc992a54
justinvdm <justinvderm@gmail.com>
2025-05-21 16:33:57 +0200

    progress

--- starters/standard/src/db.ts
+++ starters/standard/src/db.ts
@@ -3,5 +3,9 @@ import { PrismaD1 } from "@prisma/adapter-d1";
 import { env } from "cloudflare:workers";
 
 export const db = new PrismaClient({
+  // context(justinvdm, 21-05-2025): prisma-client generated type appears to
+  // consider D1 adapter incompatible, though in runtime (dev and production)
+  // it works
+  // @ts-ignore
   adapter: new PrismaD1(env.DB),
 });
