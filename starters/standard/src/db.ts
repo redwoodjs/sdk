8d6cdfdda870391df43d8bd29f552451c1189eed
Justin van der Merwe <justinvderm@gmail.com>
2025-05-21 16:47:13 +0200

    Ensure prisma client initialized after wasm engine initialized (#461)
    For prisma-client-js generator or cases where there are dynamic import to the prisma wasm modules, we need to make sure we are instantiating the prisma client later in the flow when the wasm would have been initialized.
    
    Fixes regression introduced in #443 that appears when querying in a deployed app
    
    ```
    ReferenceError: Cannot access 'UL' before initialization
    ```

--- starters/standard/src/db.ts
+++ starters/standard/src/db.ts
@@ -2,6 +2,17 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaD1 } from "@prisma/adapter-d1";
 import { env } from "cloudflare:workers";
 
-export const db = new PrismaClient({
-  adapter: new PrismaD1(env.DB),
-});
+export let db: PrismaClient;
+
+// context(justinvdm, 21-05-2025): For prisma-client-js generator or cases where
+// there are dynamic import to the prisma wasm modules, we need to make sure we
+// are instantiating the prisma client later in the flow when the wasm would
+// have been initialized.
+export const setupDb = async () => {
+  const db = new PrismaClient({
+    adapter: new PrismaD1(env.DB),
+  });
+
+  // context(justinvdm, 21-05-2025): https://github.com/cloudflare/workers-sdk/pull/8283
+  await db.$queryRaw`SELECT 1`;
+};
