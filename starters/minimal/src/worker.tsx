a9434648b635bed92fff64596d6e6ec8b72e5953
Peter Pistorius <peter.pistorius@gmail.com>
2025-06-12 17:29:04 +0200

    Remove sillyness.

--- starters/minimal/src/worker.tsx
+++ starters/minimal/src/worker.tsx
@@ -1,19 +1,17 @@
-import { defineApp, type RequestInfo } from "rwsdk/worker";
+import { defineApp } from "rwsdk/worker";
 import { render, route } from "rwsdk/router";
 
 import { Document } from "@/app/Document";
 import { Home } from "@/app/pages/Home";
 import { setCommonHeaders } from "@/app/headers";
 
-export type AppContext = {
-  dog: string,
-};
+export type AppContext = {};
 
 export default defineApp([
   setCommonHeaders(),
   ({ ctx }) => {
     // setup ctx here
-
+    ctx
   },
   render(Document, [
     route('/', Home)
