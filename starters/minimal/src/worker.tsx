d4af98ffa7a1f223995a65e31b9399582cbdd2e8
Peter Pistorius <peter.pistorius@gmail.com>
2025-06-12 17:28:00 +0200

    Update minimal starter.

--- starters/minimal/src/worker.tsx
+++ starters/minimal/src/worker.tsx
@@ -1,17 +1,21 @@
-import { defineApp } from "rwsdk/worker";
-import { index, render } from "rwsdk/router";
+import { defineApp, type RequestInfo } from "rwsdk/worker";
+import { render, route } from "rwsdk/router";
 
 import { Document } from "@/app/Document";
 import { Home } from "@/app/pages/Home";
 import { setCommonHeaders } from "@/app/headers";
 
-export type AppContext = {};
+export type AppContext = {
+  dog: string,
+};
 
 export default defineApp([
   setCommonHeaders(),
   ({ ctx }) => {
     // setup ctx here
-    ctx;
+
   },
-  render(Document, [index([Home])]),
+  render(Document, [
+    route('/', Home)
+  ])
 ]);
