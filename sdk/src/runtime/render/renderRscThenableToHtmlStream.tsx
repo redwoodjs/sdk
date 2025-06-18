665c6c683009621f5239dc06e7ab95aa4f390085
justinvdm <justinvderm@gmail.com>
2025-06-18 12:59:51 +0200

    progress

--- sdk/src/runtime/render/renderRscThenableToHtmlStream.tsx
+++ sdk/src/runtime/render/renderRscThenableToHtmlStream.tsx
@@ -16,7 +16,30 @@ export const renderRscThenableToHtmlStream = async ({
 }) => {
   const Component = () => {
     const node = (use(thenable) as { node: React.ReactNode }).node;
-    return <Document {...requestInfo}>{node}</Document>;
+
+    // todo(justinvdm, 18 Jun 2025): We can build on this later to allow users
+    // surface context. e.g:
+    // * we assign `user: requestInfo.clientCtx` here
+    // * user populates requestInfo.clientCtx on worker side
+    // * user can import a read only `import { clientCtx } from "rwsdk/client"`
+    // on client side
+    const clientContext = {
+      rw: {
+        ssr: shouldSSR,
+      },
+    };
+
+    return (
+      <Document {...requestInfo}>
+        <script
+          nonce={requestInfo.rw.nonce}
+          dangerouslySetInnerHTML={{
+            __html: `globalThis.__RWSDK_CONTEXT = ${JSON.stringify(clientContext)}`,
+          }}
+        />
+        <div id="hydrate-root">{node}</div>
+      </Document>
+    );
   };
 
   return await renderToReadableStream(<Component />, {
