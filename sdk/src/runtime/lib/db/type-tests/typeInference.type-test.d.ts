78f9d56e56b3ef999308341440dba67cf3c082b8
justinvdm <justinvderm@gmail.com>
2025-06-25 03:47:31 +0200

    progress

---
+++ sdk/src/runtime/lib/db/type-tests/typeInference.type-test.d.ts
@@ -0,0 +1,238 @@
+import type { Kysely } from "kysely";
+import type { Migration, Database } from "../typeInference";
+import type { Expect, Equal } from "./test-utils";
+
+(test = "createTable") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            username: string;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "addColumn with default value") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .addColumn("posts", "integer", (col) => col.defaultTo(0).notNull())
+          .execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            username: string;
+            posts: number;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "alterTable") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .execute();
+      },
+    },
+    "1": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .alterTable("users")
+          .addColumn("displayName", "text")
+          .execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            username: string;
+            displayName: string;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "dropTable") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .execute();
+        await db.schema
+          .createTable("posts")
+          .addColumn("title", "text")
+          .execute();
+      },
+    },
+    "1": {
+      async up(db: Kysely<any>) {
+        await db.schema.dropTable("posts").execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            username: string;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "renameTable") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .execute();
+      },
+    },
+    "1": {
+      async up(db: Kysely<any>) {
+        await db.schema.renameTable("users", "users_new").execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users_new: {
+            username: string;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "dropColumn") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .addColumn("posts", "integer", (col) => col.defaultTo(0).notNull())
+          .execute();
+      },
+    },
+    "1": {
+      async up(db: Kysely<any>) {
+        await db.schema.alterTable("users").dropColumn("posts").execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            username: string;
+          };
+        }
+      >
+    >,
+  ];
+};
+
+(test = "renameColumn") => {
+  const migrations = {
+    "0": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .createTable("users")
+          .addColumn("username", "text", (col) => col.notNull().unique())
+          .execute();
+      },
+    },
+    "1": {
+      async up(db: Kysely<any>) {
+        await db.schema
+          .alterTable("users")
+          .renameColumn("username", "handle")
+          .execute();
+      },
+    },
+  } as const;
+
+  type DB = Database<typeof migrations>;
+
+  type test = [
+    Expect<
+      Equal<
+        DB,
+        {
+          users: {
+            handle: string;
+          };
+        }
+      >
+    >,
+  ];
+};
--- sdk/src/runtime/lib/db/typeInference.ts
+++ sdk/src/runtime/lib/db/typeInference.ts
@@ -19,7 +19,7 @@ export interface ColumnBuilder<T = any> {
   onDelete(action: "cascade" | "restrict" | "set null"): ColumnBuilder<T>;
 }
 
-type ExecutedBuilder<T> = Promise<void> & { __builder_type: T };
+export type ExecutedBuilder<T> = Promise<void> & { __builder_type: T };
 
 type Prettify<T> = {
   [K in keyof T]: T[K];
@@ -41,19 +41,61 @@ export interface TableBuilder<
   execute(): ExecutedBuilder<this>;
 }
 
+type RenameProperties<T, R extends Record<string, string>> = Prettify<{
+  [P in keyof T as P extends keyof R ? R[P] : P]: T[P];
+}>;
+
 export interface AlterTableBuilder<
   TName extends string,
-  TSchema extends Record<string, any> = {},
+  TAddedColumns extends Record<string, any> = {},
+  TDroppedColumns extends string = never,
+  TRenamedColumns extends Record<string, string> = {},
 > {
   readonly __tableName: TName;
-  readonly __addedColumns: TSchema;
+  readonly __addedColumns: TAddedColumns;
+  readonly __droppedColumns: TDroppedColumns;
+  readonly __renamedColumns: TRenamedColumns;
   addColumn<K extends string, T extends string>(
     name: K,
     type: T,
     build?: (
       col: ColumnBuilder<SqlToTsType<T>>,
     ) => ColumnBuilder<SqlToTsType<T>>,
-  ): AlterTableBuilder<TName, Prettify<TSchema & Record<K, SqlToTsType<T>>>>;
+  ): AlterTableBuilder<
+    TName,
+    Prettify<TAddedColumns & Record<K, SqlToTsType<T>>>,
+    TDroppedColumns,
+    TRenamedColumns
+  >;
+  dropColumn<K extends string>(
+    name: K,
+  ): AlterTableBuilder<
+    TName,
+    TAddedColumns,
+    TDroppedColumns | K,
+    TRenamedColumns
+  >;
+  renameColumn<F extends string, T extends string>(
+    from: F,
+    to: T,
+  ): AlterTableBuilder<
+    TName,
+    TAddedColumns,
+    TDroppedColumns,
+    Prettify<TRenamedColumns & Record<F, T>>
+  >;
+  execute(): ExecutedBuilder<this>;
+}
+
+export interface DropTableBuilder<TName extends string> {
+  readonly __tableName: TName;
+  ifExists(): DropTableBuilder<TName>;
+  execute(): ExecutedBuilder<this>;
+}
+
+export interface RenameTableBuilder<TFrom extends string, TTo extends string> {
+  readonly __from: TFrom;
+  readonly __to: TTo;
   execute(): ExecutedBuilder<this>;
 }
 
@@ -65,30 +107,67 @@ export interface IndexBuilder {
 
 export interface SchemaBuilder {
   createTable<TName extends string>(name: TName): TableBuilder<TName, {}>;
-  alterTable<TName extends string>(name: TName): AlterTableBuilder<TName, {}>;
-  dropTable(name: string): any;
+  alterTable<TName extends string>(
+    name: TName,
+  ): AlterTableBuilder<TName, {}, never, {}>;
+  dropTable<TName extends string>(name: TName): DropTableBuilder<TName>;
+  renameTable<TFrom extends string, TTo extends string>(
+    from: TFrom,
+    to: TTo,
+  ): RenameTableBuilder<TFrom, TTo>;
   createIndex(name: string): IndexBuilder;
 }
 
-export type ExtractTableSchema<T> =
-  T extends TableBuilder<infer TName, infer TSchema>
-    ? Record<TName, TSchema>
-    : never;
+type ApplyBuilder<DB, B> =
+  B extends TableBuilder<infer TN, infer TS>
+    ? Prettify<DB & Record<TN, TS>>
+    : B extends AlterTableBuilder<infer TN, infer AS, infer DS, infer RS>
+      ? Prettify<
+          Omit<DB, TN> &
+            Record<
+              TN,
+              Prettify<
+                (TN extends keyof DB
+                  ? RenameProperties<Omit<DB[TN], DS>, RS>
+                  : {}) &
+                  AS
+              >
+            >
+        >
+      : B extends DropTableBuilder<infer TN>
+        ? Prettify<Omit<DB, TN>>
+        : B extends RenameTableBuilder<infer From, infer To>
+          ? Prettify<
+              Omit<DB, From> & Record<To, From extends keyof DB ? DB[From] : {}>
+            >
+          : DB;
 
-export type ExtractAlterSchema<T> =
-  T extends AlterTableBuilder<infer TName, infer TSchema>
-    ? Record<TName, TSchema>
-    : never;
+type ApplyBuilders<DB, Bs extends any[]> = Bs extends [
+  ExecutedBuilder<infer B>,
+  ...infer Rest,
+]
+  ? ApplyBuilders<ApplyBuilder<DB, B>, Rest>
+  : DB;
 
-export type MergeSchemas<A, B> = {
-  [K in keyof A | keyof B]: K extends keyof A
-    ? K extends keyof B
-      ? Prettify<A[K] & B[K]>
-      : A[K]
-    : K extends keyof B
-      ? B[K]
-      : never;
-};
+type ApplyUpResult<DB, R> =
+  R extends ExecutedBuilder<infer B>
+    ? ApplyBuilder<DB, B>
+    : R extends ExecutedBuilder<any>[]
+      ? ApplyBuilders<DB, R>
+      : DB;
+
+type ApplyMigration<DB, M extends Migration> =
+  M extends Migration<infer R> ? ApplyUpResult<DB, R> : DB;
+
+export type ComputeDatabase<
+  Migrations extends readonly Migration[],
+  DB = {},
+> = Migrations extends readonly [
+  infer M extends Migration,
+  ...infer Rest extends readonly Migration[],
+]
+  ? ComputeDatabase<Rest, ApplyMigration<DB, M>>
+  : DB;
 
 export interface MigrationDatabase {
   schema: SchemaBuilder;
@@ -100,41 +179,3 @@ export interface Migration<TUpReturn = unknown> {
 }
 
 export type Migrations = Record<string, Migration>;
-
-type GetBuilder<T> = T extends ExecutedBuilder<infer B> ? B : never;
-
-type BuildersFromMigration<TMigration extends Migration> =
-  TMigration extends Migration<infer TUpReturn>
-    ? Awaited<TUpReturn> extends Array<infer Item>
-      ? GetBuilder<Item>
-      : GetBuilder<Awaited<TUpReturn>>
-    : never;
-
-type AllBuilders<TMigrations extends Migrations> = BuildersFromMigration<
-  TMigrations[keyof TMigrations]
->;
-
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I,
-) => void
-  ? I
-  : never;
-
-type CreatedTables<TMigrations extends Migrations> = UnionToIntersection<
-  ExtractTableSchema<Extract<AllBuilders<TMigrations>, TableBuilder<any, any>>>
->;
-
-type AlteredTables<TMigrations extends Migrations> = UnionToIntersection<
-  ExtractAlterSchema<
-    Extract<AllBuilders<TMigrations>, AlterTableBuilder<any, any>>
-  >
->;
-
-type InferredDatabase<TMigrations extends Migrations> = MergeSchemas<
-  CreatedTables<TMigrations>,
-  AlteredTables<TMigrations>
->;
-
-export type Database<TMigrations extends Migrations = Migrations> = Prettify<
-  InferredDatabase<TMigrations>
->;
