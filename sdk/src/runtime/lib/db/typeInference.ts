a7f0caeaae0caeca299093f087f6484ef33038e9
justinvdm <justinvderm@gmail.com>
2025-06-26 04:44:40 +0200

    progress

--- sdk/src/runtime/lib/db/typeInference.ts
+++ sdk/src/runtime/lib/db/typeInference.ts
@@ -3,12 +3,12 @@ import { Kysely } from "kysely";
 export type SqlToTsType<T extends string> = T extends "text"
   ? string
   : T extends "integer"
-    ? number
-    : T extends "blob"
-      ? Uint8Array
-      : T extends "real"
-        ? number
-        : never;
+  ? number
+  : T extends "blob"
+  ? Uint8Array
+  : T extends "real"
+  ? number
+  : never;
 
 export interface ColumnBuilder<T = any> {
   primaryKey(): ColumnBuilder<T>;
@@ -17,6 +17,7 @@ export interface ColumnBuilder<T = any> {
   defaultTo<V extends T>(value: V): ColumnBuilder<T>;
   references(ref: string): ColumnBuilder<T>;
   onDelete(action: "cascade" | "restrict" | "set null"): ColumnBuilder<T>;
+  unsigned(): ColumnBuilder<T>;
 }
 
 type ExecutedBuilder<T> = Promise<void> & { __builder_type: T };
@@ -38,6 +39,8 @@ export interface TableBuilder<
       col: ColumnBuilder<SqlToTsType<T>>,
     ) => ColumnBuilder<SqlToTsType<T>>,
   ): TableBuilder<TName, Prettify<TSchema & Record<K, SqlToTsType<T>>>>;
+  temporary(): this;
+  ifNotExists(): this;
   execute(): ExecutedBuilder<this>;
 }
 
@@ -54,6 +57,16 @@ export interface AlterTableBuilder<
       col: ColumnBuilder<SqlToTsType<T>>,
     ) => ColumnBuilder<SqlToTsType<T>>,
   ): AlterTableBuilder<TName, Prettify<TSchema & Record<K, SqlToTsType<T>>>>;
+  dropColumn<K extends string>(
+    name: K,
+  ): AlterTableBuilder<TName, Prettify<TSchema & { [P in K]: never }>>;
+  renameColumn<KFrom extends string, KTo extends string>(
+    from: KFrom,
+    to: KTo,
+  ): AlterTableBuilder<
+    TName,
+    Prettify<TSchema & { [P in KFrom]: never } & { [P in KTo]: any }>
+  >;
   execute(): ExecutedBuilder<this>;
 }
 
@@ -64,9 +77,38 @@ export interface DropTableBuilder<TName extends string> {
   execute(): ExecutedBuilder<this>;
 }
 
+export interface CreateViewBuilder<
+  TName extends string,
+  TSchema extends Record<string, any> = {},
+> {
+  readonly __viewName: TName;
+  readonly __schema: TSchema;
+  withSchema<S extends Record<string, any>>(): CreateViewBuilder<TName, S>;
+  temporary(): this;
+  orReplace(): this;
+  ifNotExists(): this;
+  columns(columns: string[]): this;
+  as(query: any): this;
+  execute(): ExecutedBuilder<this>;
+}
+
+export interface DropViewBuilder<TName extends string> {
+  readonly __viewName: TName;
+  ifExists(): this;
+  cascade(): this;
+  execute(): ExecutedBuilder<this>;
+}
+
 export interface IndexBuilder {
   on(table: string): IndexBuilder;
   column(column: string): IndexBuilder;
+  execute(): Promise<void>;
+}
+
+export interface DropIndexBuilder {
+  on(table: string): this;
+  ifExists(): this;
+  cascade(): this;
   execute(): Promise<void>;
 }
 
@@ -75,10 +117,18 @@ export interface SchemaBuilder {
   alterTable<TName extends string>(name: TName): AlterTableBuilder<TName, {}>;
   dropTable<TName extends string>(name: TName): DropTableBuilder<TName>;
   createIndex(name: string): IndexBuilder;
+  createView<TName extends string>(name: TName): CreateViewBuilder<TName, {}>;
+  dropView<TName extends string>(name: TName): DropViewBuilder<TName>;
+  dropIndex(name: string): DropIndexBuilder;
 }
 
 export type ExtractTableSchema<T> =
   T extends TableBuilder<infer TName, infer TSchema>
+    ? Record<TName, TSchema>
+    : never;
+
+export type ExtractViewSchema<T> =
+  T extends CreateViewBuilder<infer TName, infer TSchema>
     ? Record<TName, TSchema>
     : never;
 
@@ -90,14 +140,17 @@ export type ExtractAlterSchema<T> =
 export type ExtractDroppedTableName<T> =
   T extends DropTableBuilder<infer TName> ? TName : never;
 
+export type ExtractDroppedViewName<T> =
+  T extends DropViewBuilder<infer TName> ? TName : never;
+
 export type MergeSchemas<A, B> = {
   [K in keyof A | keyof B]: K extends keyof A
     ? K extends keyof B
       ? Prettify<A[K] & B[K]>
       : A[K]
     : K extends keyof B
-      ? B[K]
-      : never;
+    ? B[K]
+    : never;
 };
 
 export interface InferenceBuilder {
@@ -136,6 +189,10 @@ type CreatedTables<TMigrations extends Migrations> = UnionToIntersection<
   ExtractTableSchema<Extract<AllBuilders<TMigrations>, TableBuilder<any, any>>>
 >;
 
+type CreatedViews<TMigrations extends Migrations> = UnionToIntersection<
+  ExtractViewSchema<Extract<AllBuilders<TMigrations>, CreateViewBuilder<any, any>>>
+>;
+
 type AlteredTables<TMigrations extends Migrations> = UnionToIntersection<
   ExtractAlterSchema<
     Extract<AllBuilders<TMigrations>, AlterTableBuilder<any, any>>
@@ -147,9 +204,33 @@ type DroppedTableNames<TMigrations extends Migrations> =
     Extract<AllBuilders<TMigrations>, DropTableBuilder<any>>
   >;
 
+type DroppedViewNames<TMigrations extends Migrations> =
+  ExtractDroppedViewName<
+    Extract<AllBuilders<TMigrations>, DropViewBuilder<any>>
+  >;
+
+type OmitNever<T> = { [K in keyof T as T[K] extends never ? never : K]: T[K] };
+
+type AllCreated<TMigrations extends Migrations> = MergeSchemas<
+  CreatedTables<TMigrations>,
+  CreatedViews<TMigrations>
+>;
+
+type MergedSchemaBeforeDrop<TMigrations extends Migrations> = MergeSchemas<
+  AllCreated<TMigrations>,
+  AlteredTables<TMigrations>
+>;
+
+type CleanedSchema<T> = {
+  [K in keyof T]: OmitNever<T[K]>;
+};
+
 type InferredDatabase<TMigrations extends Migrations> = Omit<
-  MergeSchemas<CreatedTables<TMigrations>, AlteredTables<TMigrations>>,
-  DroppedTableNames<TMigrations>
+  Omit<
+    CleanedSchema<MergedSchemaBeforeDrop<TMigrations>>,
+    DroppedTableNames<TMigrations>
+  >,
+  DroppedViewNames<TMigrations>
 >;
 
 export type Database<TMigrations extends Migrations = Migrations> = Prettify<
