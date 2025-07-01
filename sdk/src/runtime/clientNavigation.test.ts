c8375177a51ab5b0b29c3adab5354ab1a192c8c4
Peter Pistorius <peter.pistorius@gmail.com>
2025-07-01 16:56:49 +0200

    Refactor for tests.

---
+++ sdk/src/runtime/clientNavigation.test.ts
@@ -0,0 +1,9 @@
+import { initClientNavigation } from "./clientNavigation";
+import { describe, it, expect } from "vitest";
+
+describe("clientNavigation", () => {
+  // create a mock "target"
+  //   it("should be a function", () => {
+  //     expect(initClientNavigation).toBeInstanceOf(Function);
+  //   });
+});
--- sdk/src/runtime/clientNavigation.ts
+++ sdk/src/runtime/clientNavigation.ts
@@ -1,3 +1,41 @@
+export function validateClickEvent(event: MouseEvent, target: HTMLElement) {
+  // should this only work for left click?
+  if (event.button !== 0) {
+    return;
+  }
+
+  if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+    return false;
+  }
+
+  const link = target.closest("a");
+
+  if (!link) {
+    return false;
+  }
+
+  const href = link.getAttribute("href");
+  if (!href) {
+    return false;
+  }
+
+  // Skip if target="_blank" or similar
+  if (link.target && link.target !== "_self") {
+    return false;
+  }
+
+  // Skip if download attribute
+  if (link.hasAttribute("download")) {
+    return false;
+  }
+
+  if (href.startsWith("http")) {
+    return false;
+  }
+
+  return true;
+}
+
 export function initClientNavigation(
   opts: {
     onNavigate: () => void;
@@ -12,42 +50,13 @@ export function initClientNavigation(
   document.addEventListener(
     "click",
     async function handleClickEvent(event: MouseEvent) {
-      // should this only work for left click?
-      if (event.button !== 0) {
-        return;
-      }
-
-      if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-        return;
-      }
-
-      const target = event.target as HTMLElement;
-      const link = target.closest("a");
-
-      if (!link) {
-        return;
-      }
-
-      const href = link.getAttribute("href");
-      if (!href) {
-        return;
-      }
-
-      // Skip if target="_blank" or similar
-      if (link.target && link.target !== "_self") {
-        return;
-      }
-
-      // Skip if download attribute
-      if (link.hasAttribute("download")) {
-        return;
-      }
-
-      if (href.startsWith("http")) {
-        return;
-      }
-
       // Prevent default navigation
+
+      if (!validateClickEvent(event, event.target as HTMLElement)) {
+        return;
+      }
+
+      const href = (event.target as HTMLAnchorElement).href;
       event.preventDefault();
 
       // push this to the history stack.
