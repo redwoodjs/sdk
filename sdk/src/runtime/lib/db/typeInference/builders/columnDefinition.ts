d79416b57dd52ac259fba34ae931fdf02790a282
justinvdm <justinvderm@gmail.com>
2025-07-03 01:38:59 +0200

    progress

--- sdk/src/runtime/lib/db/typeInference/builders/columnDefinition.ts
+++ sdk/src/runtime/lib/db/typeInference/builders/columnDefinition.ts
@@ -1,30 +1,43 @@
-import { ColumnDefinitionBuilder as KyselyColumnDefinitionBuilder } from "kysely";
+import {
+  ColumnDefinitionBuilder as KyselyColumnDefinitionBuilder,
+  Expression,
+  sql,
+  ColumnDefinitionNode,
+} from "kysely";
 import type { Assert, AssertStillImplements } from "../assert";
 
-export interface ColumnDefinitionBuilder<T = any> {
-  primaryKey(): ColumnDefinitionBuilder<T>;
-  unique(): ColumnDefinitionBuilder<T>;
-  notNull(): ColumnDefinitionBuilder<T>;
-  unsigned(): ColumnDefinitionBuilder<T>;
-  defaultTo(value: any): ColumnDefinitionBuilder<T>;
-  references(ref: string): ColumnDefinitionBuilder<T>;
+// This is not exported from Kysely, so we have to define it ourselves
+// based on the Kysely source code.
+type DefaultValueExpression = string | number | boolean | null | typeof sql;
+
+export interface ColumnDefinitionBuilder {
+  autoIncrement(): ColumnDefinitionBuilder;
+  identity(): ColumnDefinitionBuilder;
+  primaryKey(): ColumnDefinitionBuilder;
+  references(ref: string): ColumnDefinitionBuilder;
   onDelete(
-    action: "cascade" | "restrict" | "set null" | "no action" | "set default",
-  ): ColumnDefinitionBuilder<T>;
+    onDelete: "no action" | "restrict" | "cascade" | "set null" | "set default",
+  ): ColumnDefinitionBuilder;
   onUpdate(
-    action: "cascade" | "restrict" | "set null" | "no action" | "set default",
-  ): ColumnDefinitionBuilder<T>;
-  check(expression: any): ColumnDefinitionBuilder<T>;
-  generatedAlwaysAs(expression: any): ColumnDefinitionBuilder<T>;
-  generatedAlwaysAsIdentity(options?: any): ColumnDefinitionBuilder<T>;
-  storedAs(expression: any): ColumnDefinitionBuilder<T>;
+    onUpdate: "no action" | "restrict" | "cascade" | "set null" | "set default",
+  ): ColumnDefinitionBuilder;
+  unique(): ColumnDefinitionBuilder;
+  notNull(): ColumnDefinitionBuilder;
+  unsigned(): ColumnDefinitionBuilder;
+  defaultTo(value: DefaultValueExpression): ColumnDefinitionBuilder;
+  check(expression: Expression<any>): ColumnDefinitionBuilder;
+  generatedAlwaysAs(expression: Expression<any>): ColumnDefinitionBuilder;
+  generatedAlwaysAsIdentity(): ColumnDefinitionBuilder;
+  generatedByDefaultAsIdentity(): ColumnDefinitionBuilder;
+  stored(): ColumnDefinitionBuilder;
+  modifyFront(modifier: Expression<any>): ColumnDefinitionBuilder;
+  nullsNotDistinct(): ColumnDefinitionBuilder;
+  ifNotExists(): ColumnDefinitionBuilder;
+  modifyEnd(modifier: Expression<any>): ColumnDefinitionBuilder;
   $call<T>(func: (qb: this) => T): T;
-  toOperationNode(): any;
+  toOperationNode(): ColumnDefinitionNode;
 }
 
 type _Assert = Assert<
-  AssertStillImplements<
-    ColumnDefinitionBuilder<any>,
-    KyselyColumnDefinitionBuilder
-  >
+  AssertStillImplements<ColumnDefinitionBuilder, KyselyColumnDefinitionBuilder>
 >;
--- sdk/src/runtime/lib/db/typeInference/builders/schema.ts
+++ sdk/src/runtime/lib/db/typeInference/builders/schema.ts
@@ -1,27 +1,42 @@
-import { TableBuilder } from "./table";
+import { CreateTableBuilder } from "./createTable";
 import { AlterTableBuilder } from "./alterTable";
 import { DropTableBuilder } from "./dropTable";
 import { CreateViewBuilder } from "./createView";
 import { DropViewBuilder } from "./dropView";
-import { SchemaModule as KyselySchemaBuilder } from "kysely";
+import {
+  SchemaModule as KyselySchemaBuilder,
+  CreateIndexBuilder,
+  DropIndexBuilder,
+  CreateSchemaBuilder,
+  DropSchemaBuilder,
+  CreateTypeBuilder,
+  DropTypeBuilder,
+  KyselyPlugin,
+  RefreshMaterializedViewBuilder,
+} from "kysely";
 import type { Assert, AssertStillImplements } from "../assert";
 
 export interface SchemaBuilder {
-  createTable<TName extends string>(name: TName): TableBuilder<TName, {}>;
+  createTable<TName extends string>(
+    name: TName,
+  ): CreateTableBuilder<TName, never>;
   alterTable<TName extends string>(name: TName): AlterTableBuilder<TName, {}>;
   dropTable<TName extends string>(name: TName): DropTableBuilder<TName>;
-  createView<TName extends string>(name: TName): CreateViewBuilder<TName, {}>;
+  createView<TName extends string>(
+    name: TName,
+  ): CreateViewBuilder<TName, never>;
   dropView<TName extends string>(name: TName): DropViewBuilder<TName>;
-  alterView(name: string): any;
   withSchema(schema: string): this;
-  createIndex(name: string): any;
-  dropIndex(name: string): any;
-  createSchema(name: string): any;
-  dropSchema(name: string): any;
-  createType(name: string): any;
-  dropType(name: string): any;
-  toOperationNode(): any;
+  createIndex(name: string): CreateIndexBuilder;
+  dropIndex(name: string): DropIndexBuilder;
+  createSchema(name: string): CreateSchemaBuilder;
+  dropSchema(name: string): DropSchemaBuilder;
+  createType(name: string): CreateTypeBuilder;
+  dropType(name: string): DropTypeBuilder;
+  refreshMaterializedView(viewName: string): RefreshMaterializedViewBuilder;
   $call<T>(func: (qb: this) => T): T;
+  withPlugin(plugin: KyselyPlugin): this;
+  withoutPlugins(): this;
 }
 
 type _Assert = Assert<
