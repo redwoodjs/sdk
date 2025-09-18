864555e48bdfb0eae4f2b8e476be63fd21789717
justinvdm <justinvderm@gmail.com>
2025-09-18 05:27:24 +0200

    Add bootstrap module to renderToReadableStream options.

--- sdk/src/runtime/render/renderRscThenableToHtmlStream.tsx
+++ sdk/src/runtime/render/renderRscThenableToHtmlStream.tsx
@@ -60,6 +60,7 @@ export const renderRscThenableToHtmlStream = async ({
 
   return await renderToReadableStream(<Component />, {
     nonce: requestInfo.rw.nonce,
+    bootstrapModules: ["/src/client.tsx"], // EXPERIMENT 1
     onError(error, { componentStack }) {
       try {
         if (!error) {
