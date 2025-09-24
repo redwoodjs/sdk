2c57bc3bf6d602fa966e075c6b5c78e86ef92ede
justinvdm <justinvderm@gmail.com>
2025-09-24 16:39:15 +0200

    Squashed commit of the following:
    commit 324946130ec7fdc73294c0b617726f4a07478c8c
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 16:32:06 2025 +0200
    
        Update Content-Security-Policy to allow data: URLs.
    
    commit 3186312e65e3a51db9f2cdc280dfbb359ed5a78f
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 16:21:41 2025 +0200
    
        Set timeout values from environment variables.
    
    commit 5a0ecee01cd73d24a28bd15ea3641f17f23dd08d
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 16:20:23 2025 +0200
    
        Remove favicon files and replace with inline SVG icons.
    
    commit 31e5b19a6d63a907285c1e91835de4df03fe03aa
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 16:12:17 2025 +0200
    
        Update regex patterns to match server and client IDs.
    
    commit 05125ffd3d7552d242587d596488107e64cc4a3e
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 16:04:42 2025 +0200
    
        Use waitForSelector instead of waitForXPath.
    
    commit a35d04ab74298cad3c3ea4ea67b45aef8cb88bf4
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 15:59:26 2025 +0200
    
        Update imports to use waitForHydration.
    
    commit a2189d7437295fd422701461bf212ba63f7625fa
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 15:28:15 2025 +0200
    
        Remove outdated e2e test convention documentation.
    
    commit 13045c849c7e39cee4540f44579e2a6c11df5f2d
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 15:28:00 2025 +0200
    
        Implement e2e test hydration conventions and utility.
    
    commit 91cce22a7c764bdbea9f7428c1810fbee750712a
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 15:09:27 2025 +0200
    
        Fix monorepo workspace resolution in e2e harness.
    
    commit 9b8f17914d7fdafa93093aed89f2bbfd5dfa55ac
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 14:55:56 2025 +0200
    
        Make the test harness monorepo-aware.
    
    commit 0bccd940637889cb9a61dda5e7730a7b91d00f4a
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 14:26:33 2025 +0200
    
        Update matrix generation in GitHub workflows.
    
    commit 59932d22736653530aa9320273bf3b8afc2eb1d1
    Author: justinvdm <justinvderm@gmail.com>
    Date:   Wed Sep 24 14:20:33 2025 +0200
    
        _

--- playground/shadcn/src/app/Document.tsx
+++ playground/shadcn/src/app/Document.tsx
@@ -12,15 +12,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <link rel="stylesheet" href={stylesUrl} />
       <link
         rel="icon"
-        type="image/svg+xml"
-        href="/favicon-dark.svg"
-        media="(prefers-color-scheme: dark)"
-      />
-      <link
-        rel="icon"
-        type="image/svg+xml"
-        href="/favicon-light.svg"
-        media="(prefers-color-scheme: light)"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚫️</text></svg>"
       />
     </head>
     <body>
