e99b6cd1bee1a61ae3bd422784943d3ff4ea59ad
justinvdm <justinvderm@gmail.com>
2025-10-02 04:41:01 +0200

    Make durable object binding type more flexible.

--- sdk/src/runtime/lib/db/createDb.ts
+++ sdk/src/runtime/lib/db/createDb.ts
@@ -1,11 +1,11 @@
 import { Kysely } from "kysely";
 import { DOWorkerDialect } from "./DOWorkerDialect.js";
-import { type SqliteDurableObject } from "./index.js";
 
-export function createDb<T, DurableObject extends SqliteDurableObject>(
-  durableObjectBinding: DurableObjectNamespace<DurableObject>,
-  name = "main",
-): Kysely<T> {
+export function createDb<
+  T,
+  DurableObjectBinding extends
+    DurableObjectNamespace<any> = DurableObjectNamespace<any>,
+>(durableObjectBinding: DurableObjectBinding, name = "main"): Kysely<T> {
   return new Kysely<T>({
     dialect: new DOWorkerDialect({
       kyselyExecuteQuery: (...args) => {
