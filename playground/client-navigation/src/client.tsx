ca80ea91c3dddb1d0f5d7d09f25282c4639c257c
justinvdm <justinvderm@gmail.com>
2025-10-02 19:00:28 +0200

    Update client initialization to use navigation features.

--- playground/client-navigation/src/client.tsx
+++ playground/client-navigation/src/client.tsx
@@ -1,3 +1,4 @@
-import { initClient } from "rwsdk/client";
+import { initClient, initClientNavigation } from "rwsdk/client";
 
-initClient();
+const { handleResponse } = initClientNavigation();
+initClient({ handleResponse });
