f6c99fad9b744387e7de465c323fd3ad8aa0aacc
justinvdm <justinvderm@gmail.com>
2025-10-02 10:15:37 +0200

    Remove unused type parameter

--- playground/database-do/src/db/db.ts
+++ playground/database-do/src/db/db.ts
@@ -5,7 +5,4 @@ import { type Database, createDb } from "rwsdk/db";
 export type AppDatabase = Database<typeof migrations>;
 export type Todo = AppDatabase["todos"];
 
-export const db = createDb<AppDatabase>(
-  env.APP_DURABLE_OBJECT,
-  "todo-database",
-);
+export const db = createDb(env.APP_DURABLE_OBJECT, "todo-database");
--- sdk/src/runtime/lib/db/createDb.ts
+++ sdk/src/runtime/lib/db/createDb.ts
@@ -1,12 +1,20 @@
+import { DurableObject } from "cloudflare:workers";
 import { Kysely } from "kysely";
 import { DOWorkerDialect } from "./DOWorkerDialect.js";
+import { Database } from "./typeInference/database.js";
 
-export function createDb<
-  T,
-  DurableObjectBinding extends
-    DurableObjectNamespace<any> = DurableObjectNamespace<any>,
->(durableObjectBinding: DurableObjectBinding, name = "main"): Kysely<T> {
-  return new Kysely<T>({
+type MigrationsFromDurableObjectClass<
+  DurableObjectClass extends DurableObject,
+> =
+  DurableObjectClass extends SqliteDurableObject<infer DB>
+    ? DB["migrations"]
+    : never;
+
+export function createDb<DatabaseDurableObject extends DurableObject>(
+  durableObjectBinding: DurableObjectNamespace<DatabaseDurableObject>,
+  name = "main",
+): Kysely<Database<MigrationsFromDurableObjectClass<DatabaseDurableObject>>> {
+  return new Kysely<Database>({
     dialect: new DOWorkerDialect({
       kyselyExecuteQuery: (...args) => {
         const durableObjectId = durableObjectBinding.idFromName(name);
