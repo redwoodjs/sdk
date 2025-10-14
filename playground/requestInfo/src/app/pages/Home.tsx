3bb93c13e29a18c8a90dbd78e8f6a010f2ea5e4e
justinvdm <justinvderm@gmail.com>
2025-10-14 02:20:46 +0200

    Replace ActionButtonComponent with ActionFormComponent.

--- playground/requestInfo/src/app/pages/Home.tsx
+++ playground/requestInfo/src/app/pages/Home.tsx
@@ -14,7 +14,7 @@ export function Home({ ctx, request }: RequestInfo) {
       {/* <ClientComponent /> */}
       {/* <ServerComponent /> */}
       {/* <PlainComponent /> */}
-      {/* <ActionButtonComponent /> */}
+      {/* <ActionFormComponent /> */}
     </div>
   );
 }
