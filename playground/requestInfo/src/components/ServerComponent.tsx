e86c1eea6bc4b5311e6477b3430b623fddc61572
justinvdm <justinvderm@gmail.com>
2025-10-21 03:00:54 +0200

    Fix whitespace in ServerComponent.

--- playground/requestInfo/src/components/ServerComponent.tsx
+++ playground/requestInfo/src/components/ServerComponent.tsx
@@ -2,7 +2,7 @@ export const ServerComponent = () => {
   return (
     <div>
       <h2>Server Component</h2>
-      <p>Is 2 even? {/* isEven(2) ? "Yes" : "No" */}</p>
+      <p>Is 2 even? {/* {isEven(2) ? "Yes" : "No"} */}</p>
     </div>
   );
 };
