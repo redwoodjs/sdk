757a09478330cc9cd89a76cb4cf9cf1a414635f2
Peter Pistorius <peter.pistorius@gmail.com>
2025-11-15 11:05:05 +0200

    Fix exports.

--- sdk/src/use-server-state/index.ts
+++
@@ -1,8 +0,0 @@
-export { initSyncStateClient } from "./client";
-export { SyncStateServer, type SyncStateValue } from "./SyncStateServer.mjs";
-export {
-  createSyncStateHook,
-  useSyncedState,
-  type CreateSyncStateHookOptions,
-} from "./useSyncedState";
-export { syncStateRoutes, type SyncStateRouteOptions } from "./worker.mjs";
