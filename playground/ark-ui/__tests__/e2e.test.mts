078a8cf527a0fc0dd62b90a134fcbbd7231b0d88
Adebesin Tolulope <adebesintolulope80@gmail.com>
2026-01-04 00:06:08 +0100

    feat: Add RedwoodSDK Minimal Starter files

---
+++ playground/ark-ui/__tests__/e2e.test.mts
@@ -0,0 +1,16 @@
+import { expect } from "vitest";
+import { setupPlaygroundEnvironment, testDevAndDeploy, poll } from "rwsdk/e2e";
+
+setupPlaygroundEnvironment(import.meta.url);
+
+testDevAndDeploy("renders Hello World", async ({ page, url }) => {
+  await page.goto(url);
+
+  const getPageContent = () => page.content();
+
+  await poll(async () => {
+    const content = await getPageContent();
+    expect(content).toContain("Hello World");
+    return true;
+  });
+});
---
+++ playground/ark-ui/src/app/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { RequestInfo } from "rwsdk/worker";
+import { Accordion } from '@ark-ui/react/accordion'
+// import { ChevronDownIcon } from 'lucide-react'
+
+export function Home({ ctx }: RequestInfo) {
+  return <div>Hello World
+
+    <Accordion.Root defaultValue={['React']}>
+      {['React', 'Solid', 'Vue', 'Svelte'].map((item) => (
+        <Accordion.Item key={item} value={item}>
+          <Accordion.ItemTrigger>
+            What is {item}?
+            {/* <Accordion.ItemIndicator>
+              <ChevronDownIcon />
+            </Accordion.ItemIndicator> */}
+          </Accordion.ItemTrigger>
+          <Accordion.ItemContent>{item} is a JavaScript library for building user interfaces.</Accordion.ItemContent>
+        </Accordion.Item>
+      ))}
+    </Accordion.Root>
+  </div>;
+}
