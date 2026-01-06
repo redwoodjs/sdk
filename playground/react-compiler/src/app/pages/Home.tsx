3d46825af8fe3f9f2dde48fb489426156287e287
justinvdm <justinvderm@gmail.com>
2026-01-06 11:31:20 +0200

    Rename Button.client.tsx to Button.tsx and add Button import to Home.tsx.

--- playground/react-compiler/src/app/pages/Home.tsx
+++ playground/react-compiler/src/app/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { RequestInfo } from "rwsdk/worker";
+import { Button } from "./Button.js";
 
 export function Home({ ctx }: RequestInfo) {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button />
     </div>
   );
 }
